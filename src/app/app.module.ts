7ade9927eafc65ddb45c99ea3343424d93099041
Cabidge <cabbageboi69@gmail.com>
2021-08-21 20:37:20 -0400

    Created hangman component and added to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HangmanComponent } from './hangman/hangman.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HangmanComponent
   ],
   imports: [
     BrowserModule
